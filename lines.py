e19ed8db060112e6dd5f246aac4986585140e393
Malo Mn <malo.maisonneuve@gmail.com>
2021-01-26 19:54:05 +0100

    Photo & Bgn of recursive _longest_line

--- lines.py
+++ lines.py
@@ -9,7 +9,7 @@ original = cv.imread('photos/line.png', 0)
 _, thresh = cv.threshold(original, 75, 255, cv.THRESH_BINARY)
 thresh = cv.bitwise_not(thresh)
 
-cv.imshow("img", thresh)
+# cv.imshow("img", thresh)
 
 
 def redraw_shape(img):
@@ -26,41 +26,33 @@ def redraw_shape(img):
 
 
 thresh = redraw_shape(thresh)
-cv.imshow('redrawn', thresh)
+# cv.imshow('redrawn', thresh)
 
-
-def find_skeleton(img):
-    # Step 1: Create an empty skeleton
-    size = np.size(img)
-    skeleton = np.zeros(img.shape, np.uint8)
-
-    # Get a Cross Shaped Kernel
-    element = cv.getStructuringElement(cv.MORPH_CROSS, (3, 3))
-
-    # Repeat steps 2-4
-    while True:
-        # Step 2: Open the image
-        open = cv.morphologyEx(img, cv.MORPH_OPEN, element)
-        # Step 3: Subtract open from the original image
-        temp = cv.subtract(img, open)
-        # Step 4: Erode the original image and refine the skeleton
-        eroded = cv.erode(img, element)
-        skeleton = cv.bitwise_or(skeleton, temp)
-        img = eroded.copy()
-        # Step 5: If there are no white pixels left ie.. the image has been completely eroded, quit the loop
-        if cv.countNonZero(img) == 0:
-            break
-
-    return skeleton
-
-
-# line = find_skeleton(thresh)
+# Extracting the lines from the
 line = cv.ximgproc.thinning(thresh)
-cv.imshow("Skeleton", line)
+# cv.imshow("Skeleton", line)
 
 
 def get_longest_line(lines):
-    
+    indexes = np.where(lines == 255)
+    pts = []
+    for i, j in zip(*indexes):
+        pts.append([i, j])
+
+    pts.sort(key=lambda a: sum(a))
+
+    start = pts[0]
+    pts.remove(start)
+
+    return _longest_line(start, pts, 1)
+
+
+def _longest_line(point, points, length):
+    closest_points = filter(lambda a: abs(a[0] - point[0]) + abs(a[1] - point[1]), points)
+
+
+print(get_longest_line(line))
+
 
 cv.waitKey(0)
 cv.destroyAllWindows()
